abdc1c570150e2f7f6feac1f7411ee616285c02f
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-03-30 23:56:52 -0500

    added code to jumbotron index

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
