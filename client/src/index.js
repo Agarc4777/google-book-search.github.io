23c725d64505c708ba15fdba8a4bff6d3f2dda7b
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-04-02 14:44:25 -0500

    deleted everything

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<app />, document.getElementById('root'));
